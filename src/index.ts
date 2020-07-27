c2d602dff505075ad48235335f5692c8ad409810
Juan Manuel Servera Bondroit <juanserv@microsoft.com>
2020-07-27 12:17:24 +0200

    Add deploy from git

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,14 @@ import { Server } from "./server";
 import dotenv from "dotenv";
 dotenv.config();
 
-const stunServer:string = process.env.STUNSERVER ? process.env.STUNSERVER : "";
-const turnServer:string = process.env.TURNSERVER?process.env.TURNSERVER:"";
-const turnUser:string = process.env.TURNUSER?process.env.TURNUSER:"";
-const turnKey:string = process.env.TURNKEY?process.env.TURNKEY:"";
+const stunServer: string = process.env.STUNSERVER ? process.env.STUNSERVER : "";
+const turnServer: string = process.env.TURNSERVER ? process.env.TURNSERVER : "";
+const turnUser: string = process.env.TURNUSER ? process.env.TURNUSER : "";
+const turnKey: string = process.env.TURNKEY ? process.env.TURNKEY : "";
+const port: number = process.env.PORT ? parseInt(process.env.PORT) : Server.DEFAULT_PORT;
 
-const server = new Server(stunServer,turnServer, turnUser, turnKey);
- 
-server.listen(port => {
- console.log(`Server is listening on http://localhost:${port}`);
+const server = new Server(stunServer, turnServer, turnUser, turnKey);
+
+server.listen(port, port => {
+    console.log(`Server is listening on http://localhost:${port}`);
 });
--- src/server.ts
+++ src/server.ts
@@ -7,14 +7,14 @@ import util from "util";
 import crypto from "crypto";
 import fs from "fs";
 
-export interface IIceServer{
-  urls?:string,
-  credential?:string,
-  username?:string
+export interface IIceServer {
+  urls?: string,
+  credential?: string,
+  username?: string
 };
 
-export interface IIceConfig{
-  iceServers?:Array<IIceServer>
+export interface IIceConfig {
+  iceServers?: Array<IIceServer>
 }
 
 export class Server {
@@ -23,20 +23,20 @@ export class Server {
   private app: Application;
   private io: SocketIOServer;
 
-  private turnUser:string;
-  private turnKey:string;
-  private turnServer:string;
-  private stunServer:string;
+  private turnUser: string;
+  private turnKey: string;
+  private turnServer: string;
+  private stunServer: string;
 
   private activeSockets: string[] = [];
 
-  private readonly DEFAULT_PORT = 8080;
+  static DEFAULT_PORT = 8080;
 
-  constructor(stunServer:string, turnServer:string, user:string,key:string) {
-    this.stunServer=stunServer;
-    this.turnServer=turnServer;
-    this.turnUser=user;
-    this.turnKey=key;
+  constructor(stunServer: string, turnServer: string, user: string, key: string) {
+    this.stunServer = stunServer;
+    this.turnServer = turnServer;
+    this.turnUser = user;
+    this.turnKey = key;
     this.initialize();
   }
 
@@ -55,49 +55,49 @@ export class Server {
   }
 
 
-  private getTURNCredentials(name:string, secret:string):{username:string,password:string}{    
-    var unixTimeStamp:Number = Math.round(Date.now()/1000) + 24*3600,  // this credential would be valid for the next 24 hours
-        username:string = [unixTimeStamp, name].join(':'), password:string,
-        hmac = crypto.createHmac('sha1', secret);
+  private getTURNCredentials(name: string, secret: string): { username: string, password: string } {
+    var unixTimeStamp: Number = Math.round(Date.now() / 1000) + 24 * 3600,  // this credential would be valid for the next 24 hours
+      username: string = [unixTimeStamp, name].join(':'), password: string,
+      hmac = crypto.createHmac('sha1', secret);
     hmac.setEncoding('base64');
     hmac.write(username);
     hmac.end();
     password = hmac.read();
     return {
-        username: username,
-        password: password
+      username: username,
+      password: password
     };
-}
+  }
 
   private configureRoutes(): void {
     this.app.get("/", (req, res) => {
       res.sendFile("index.html");
     });
-    this.app.get("/js/index.js", (req,res)=>{
-      let data=fs.readFileSync('js/index.js');
-      if(data){
-        var ice_config:IIceConfig={};
-        if(this.turnServer){          
-          ice_config.iceServers=[];
+    this.app.get("/js/index.js", (req, res) => {
+      let data = fs.readFileSync('js/index.js');
+      if (data) {
+        var ice_config: IIceConfig = {};
+        if (this.turnServer) {
+          ice_config.iceServers = [];
 
-          var iceServer:IIceServer={urls:this.turnServer};
-          if(this.turnUser){
-            var credentials= this.getTURNCredentials(this.turnUser, this.turnKey);
-            iceServer.username=credentials.username;
-            iceServer.credential=credentials.password;            
+          var iceServer: IIceServer = { urls: this.turnServer };
+          if (this.turnUser) {
+            var credentials = this.getTURNCredentials(this.turnUser, this.turnKey);
+            iceServer.username = credentials.username;
+            iceServer.credential = credentials.password;
           }
           ice_config.iceServers.push(iceServer);
         }
 
-        if(this.stunServer){
-          if(ice_config.iceServers==null){
-            ice_config.iceServers=[];
+        if (this.stunServer) {
+          if (ice_config.iceServers == null) {
+            ice_config.iceServers = [];
           }
-          var iceServer:IIceServer={urls:this.stunServer};
+          var iceServer: IIceServer = { urls: this.stunServer };
           ice_config.iceServers.push(iceServer);
         }
-        var script:string = data.toString().replace('{{ice_config}}', util.inspect(ice_config) );
-        res.setHeader("Content-Type","text/javascript");
+        var script: string = data.toString().replace('{{ice_config}}', util.inspect(ice_config));
+        res.setHeader("Content-Type", "text/javascript");
         res.send(script);
       }
 
@@ -155,9 +155,9 @@ export class Server {
     });
   }
 
-  public listen(callback: (port: number) => void): void {
-    this.httpServer.listen(this.DEFAULT_PORT, () => {
-      callback(this.DEFAULT_PORT);
+  public listen(port: number = Server.DEFAULT_PORT, callback: (port: number) => void): void {
+    this.httpServer.listen(port, () => {
+      callback(port);
     });
   }
 }
